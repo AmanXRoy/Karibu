c5ab8120fe454bf84a3ffd2ee769f04a98929a67
amanroy007 <roya5846@gmail.com>
2025-01-17 00:49:56 +0530

    shadcn ui library added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        'primary': '#FFA500',
-        'secondary': '#1E1E1E',
-        'secondary-light': '#333333',
-        'success': '#28A745',
-      },
-
-    },
+  	extend: {
+  		colors: {
+  			primary: '#FFA500',
+  			secondary: '#1E1E1E',
+  			'secondary-light': '#333333',
+  			success: '#28A745'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 
